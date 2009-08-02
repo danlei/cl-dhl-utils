165026a02c87c7828a25a22f5307cdce066e18ea
Daniel H. Leidisch <public@leidisch.net>
2009-08-02 09:23:17 +0200

    Removed lisp-unit from system.

--- tests/packages.lisp
+++ tests/packages.lisp
@@ -1,35 +1,11 @@
 ;;;;;
 ;;;;; packages.lisp
 ;;;;;
-;;;;; Time-stamp: <2009-04-25 23:56:00>
+;;;;; Time-stamp: <2009-08-02 06:23:05 danlei>
 ;;;;;
 
 
 (in-package #:common-lisp-user)
-
-(defpackage #:lisp-unit
-  (:use #:common-lisp)
-  (:export #:define-test
-	   #:run-all-tests
-	   #:run-tests
-           #:assert-eq
-	   #:assert-eql
-	   #:assert-equal
-	   #:assert-equalp
-           #:assert-error
-	   #:assert-expands
-	   #:assert-false
-           #:assert-equality
-	   #:assert-prints
-	   #:assert-true
-           #:get-test-code
-	   #:get-tests
-           #:remove-all-tests
-	   #:remove-tests
-           #:logically-equal
-	   #:set-equal
-           #:use-debugger
-           #:with-test-listener))
 
 (defpackage #:dhl-utils-test
   (:documentation "Test package for dhl-utils.")
